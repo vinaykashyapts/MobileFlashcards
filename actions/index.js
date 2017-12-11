94a475213bab04a79db8f61a797900ddbf411cda
Vinay <vinay@cropin.in>
2017-12-11 18:19:02 +0530

    added actions

---
+++ actions/index.js
@@ -0,0 +1,51 @@
+export const RECEIVE_DECKS = 'RECEIVE_DECKS';
+export const GET_DECK = 'GET_DECK';
+export const ADD_DECK = 'ADD_DECK';
+export const ADD_CARD = 'ADD_CARD';
+
+/**
+ * Receive all decks
+ * @param decks
+ * @returns {{type: string, decks: *}}
+ */
+export function receiveDecks (decks) {
+  return {
+    type: RECEIVE_DECKS,
+    decks,
+  }
+}
+
+/**
+ * Adds a specific deck
+ * @param deck
+ * @returns {{type: string, deck: *}}
+ */
+export function addDeck (deck) {
+  return {
+    type: ADD_DECK,
+    deck,
+  }
+}
+
+/**
+ * Adds a specific card
+ * @returns {{type: string}}
+ */
+export function addCard(send) {
+  return {
+    type: ADD_CARD,
+    send,
+  }
+}
+
+/**
+ * Gets a single deck
+ * @param deck
+ * @returns {{type: string, deck: *}}
+ */
+export function getSingleDeck (deck) {
+  return {
+    type: GET_DECK,
+    deck,
+  }
+}
